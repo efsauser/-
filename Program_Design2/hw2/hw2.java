705779b6bd224ed8042db4c1ff0638e4e136985d
efsauser <40321412+efsauser@users.noreply.github.com>
2024-03-30 21:48:32 +0800

    Update hw2.java
    完成了最後的讓我可以通過刁鑽測資的修正

--- Program_Design2/hw2/hw2.java
+++ Program_Design2/hw2/hw2.java
@@ -1,9 +1,10 @@
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
+
 import java.lang.String;
 import java.util.regex.*;
 
@@ -17,13 +18,14 @@ public class CodeGenerator {
 
         try {
             BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            // 讀取"classDiagram"然後不做任何事
+            // 讀取 "classDiagram" 然後不做任何事
             mermaidLine = reader.readLine();
             // 一行一行讀取文件(借用RegExp的內容)
-            while ((mermaidLine = reader.readLine()) != null) {
+            while ((mermaidLine = reader.readLine()) != null){
                 // Parser.lineTest(mermaidLine);
                 lineCategory = lineParser.classifier(mermaidLine);
                 if(lineCategory.equals("class")){
+                    // 如果不是第一個class，就在前一個class寫下右大括號然後建立新的class檔
                     if(!CodeWriter.fileName.equals("(>_<)")){
                         CodeWriter.writeRightBrace(lineParser.className);
                     }
@@ -36,17 +38,14 @@ public class CodeGenerator {
                         CodeWriter.attributeWrite( lineParser.className, lineParser.type, 
                                                    lineParser.name, security);
                     }else if(lineCategory.equals("getter")){
-                        String security = Pattern.matches(".*: \\+.*", mermaidLine)? "public": "private";
                         lineParser.getterParser(mermaidLine);
                         CodeWriter.getterWrite( lineParser.className, lineParser.type, 
-                                                lineParser.name, security);
-                    }
-                    else if(lineCategory.equals("setter")){
+                                                lineParser.name);
+                    }else if(lineCategory.equals("setter")){
                         lineParser.setterParser(mermaidLine);
                         CodeWriter.setterWrite( lineParser.className, lineParser.type, 
                                                 lineParser.name, lineParser.parameter);
-                    }
-                    else if(lineCategory.equals("method")){
+                    }else if(lineCategory.equals("method")){
                         String security = Pattern.matches(".*: \\+.*", mermaidLine)? "public": "private";
                         lineParser.methodParser(mermaidLine);
                         CodeWriter.methodWrite( lineParser.className, lineParser.type, 
@@ -61,7 +60,8 @@ public class CodeGenerator {
                 //                 "| category:" +lineCategory+
                 //                 "| type:"+lineParser.type+
                 //                 "| name:"+lineParser.name);
-            }  
+            }
+            // 讀到EOF也要寫下右大括號
             CodeWriter.writeRightBrace(lineParser.className);
             reader.close();
         } catch (IOException e) {
@@ -72,7 +72,6 @@ public class CodeGenerator {
 
 class myParser{
     public String className;
-    public boolean isClass;
     public String type; // identifierName
     public String name; // identifierType
     public String parameter; // parameter of setter or simple method(不包含setter或getter) 
@@ -107,18 +106,19 @@ class myParser{
         type =  type.replace("+", "");
         type =  type.replace("-", "");
     }
-    // 重要!!! 準備來處理大小寫問題
+
     public void getterParser(String inputLine){
         String[] words = inputLine.split(" ");
         type = words[words.length-1];
         name = words[words.length-2];
+        // 切出getXXX的XXX
         name = name.substring(4);
     }
 
     public void setterParser(String inputLine){
+        // 切出setXXX的XXX <- 為什麼VScode會覺得這行註解有問題? ( XXX Java(536871362) )
         String[] temp1 = inputLine.split("\\+");
         String[] temp2 = temp1[temp1.length-1].split("\\(");
-        // 切出getXXX
         type = "void";
         name = temp2[0];
         // 切出parameter，整個括號裡都切出來
@@ -133,7 +133,7 @@ class myParser{
         String[] temp2 = temp1[temp1.length-1].split("\\(");
         // 切出method的name
         if(Pattern.matches(".*\\).*", words[words.length-1])){
-            // 如果method的)「竟然」沒有東西的話，則視return為void
+            // 如果method的)後「竟然」沒有東西的話，則視為要return void
             // 六個測資都沒有這個情況，難道陷阱藏在隱藏測資裡?
             type = "void";
         }else{
@@ -148,15 +148,14 @@ class myParser{
 }
 
 class myFileWriter{
+    // 用來標記是不是正在寫入第一個class
+    // 有趣又有效，對吧?
     public String fileName = "(>_<)";
 
     public void writeRightBrace(String className){
         try {
             String fileName = className + ".java";
             File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
                 bw.write("}\n");
             }
@@ -181,13 +180,10 @@ class myFileWriter{
     }
 
     public void attributeWrite( 
-    String className, String attributeType, String attributeName, String security ){
+    String className, String attributeType, String attributeName, String security){
         try{
             fileName = className + ".java";
             File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
                 bw.write("\s\s\s\s"+ security + " " + attributeType + " " + attributeName + ";\n");
             }
@@ -197,17 +193,19 @@ class myFileWriter{
     }
 
     public void getterWrite( 
-    String className, String getterType, String getterName, String security ){
+    String className, String getterType, String getterName){
         try{
             fileName = className + ".java";
             File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
-                bw.write("\s\s\s\s"+ security + " " + getterType + " get" + getterName + " {\n" 
-                        + "\s\s\s\s\s\s\s\sreturn " + getterName.substring(0,getterName.length()-2).toLowerCase() 
-                        + ";\n\s\s\s\s}\n");
+                // 先經歷一個麻煩的首字母轉小寫過程
+                String firstLetter = getterName.substring(0,1);
+                String lowerCaseLetter = firstLetter.toLowerCase();
+                String get = lowerCaseLetter + getterName.substring(1);
+                getterName = getterName.substring(0,getterName.length()-2);
+                get = get.substring(0,get.length()-2);
+                bw.write("\s\s\s\spublic" + " " + getterType + " get" + getterName + "() {\n" 
+                        + "\s\s\s\s\s\s\s\sreturn " + get + ";\n\s\s\s\s}\n");
             }
         }catch (IOException e) {
             e.printStackTrace();
@@ -215,13 +213,10 @@ class myFileWriter{
     }
 
     public void setterWrite( 
-    String className, String setterType, String setterName, String parameter ){
+    String className, String setterType, String setterName, String parameter){
         try{
             fileName = className + ".java";
             File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
                 String[] temp = parameter.split(" ");
                 String set = temp[temp.length - 1];
@@ -233,15 +228,12 @@ class myFileWriter{
         }
     }
 
-    // 都寫完了才發現是不是這裡繼承自setterWrite就好
+    // 都寫完了才發現是不是這裡可以繼承自setterWrite就好
     public void methodWrite( 
     String className, String methodType, String methodName, String parameter, String security){
         try{
             fileName = className + ".java";
             File file = new File(fileName);
-            if (!file.exists()) {
-                file.createNewFile();
-            }
             try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
                 bw.write("\s\s\s\spublic "+ methodType + " " + methodName +"("+ parameter + ") ");
                 if(methodType.equals("int")){
