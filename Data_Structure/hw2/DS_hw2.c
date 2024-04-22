64f4a676a1687bca7c20e7381836aef7e0e1417c
efsauser <40321412+efsauser@users.noreply.github.com>
2024-04-22 23:05:00 +0800

    Add files via upload

---
+++ Data_Structure/hw2/DS_hw2.c
@@ -0,0 +1,134 @@
+﻿#include <stdio.h>
+#include <string.h>
+#define MAX 10000
+int parent[MAX];
+
+int find(int i){
+    for(; parent[i]>=0; i=parent[i])
+    ;
+    return i;
+}
+
+void heightUnion(int i, int j){
+    //i, j are roots with parent[i] = -height[i]
+    int root1 = find(i), root2 = find(j);
+    if(root1 > root2) // ex: -5 > -10
+        parent[root1] = root2;
+    else if (root1 == root2){
+        parent[root2] = root1;
+        parent[root1]--;
+    }
+    else
+        parent[root2] = root1;
+}
+
+int printSame(int e1, int e2){
+    if(find(e1)==find(e2))
+        printf("true");
+    else 
+        printf("false");
+}
+
+void printParent(){
+    for(int i=0; i<10; i++)
+        printf("%2d ", i);
+    printf("\n");
+    for(int i=0; i<10; i++)
+        printf("%2d ", parent[i]);
+    printf("\n");
+}
+
+int main(){
+    int cases, n, ops, e1, e2;
+    char operation[MAX];
+    scanf("%d", &cases);
+    for(int i=0; i<cases; i++){ // loop of cases
+        scanf("%d %d", &n, &ops);
+        for(int i=0; i<n; i++) // initialize
+            parent[i] = -1;
+        for(int j=0; j<ops; j++){ // loop of ops
+            scanf("%s", &operation);
+            if(strcmp(operation, "union")==0){
+                scanf("%d %d", &e1, &e2);
+                heightUnion(e1, e2);
+            }
+            if(strcmp(operation, "find")==0){
+                scanf("%d", &e1);
+                printf("%d\n", find(e1));
+            }
+            if(strcmp(operation, "same")==0){
+                scanf("%d %d", &e1, &e2);
+                if(find(e1)==find(e2))
+                    printf("true\n");
+                else 
+                    printf("false\n");
+            }
+        }
+    }
+    return 0;
+}
+
+
+// #include <stdio.h>
+// #include <stdlib.h>
+
+// int *parent;
+
+// int find(int i) {
+//     for(; parent[i]>=0; i=parent[i])
+//     ;
+//     return i;
+// }
+
+// void heightUnion(int a, int b) {
+//     int rootA = find(a);
+//     int rootB = find(b);
+
+//     if (rootA != rootB) {
+//         if (parent[rootA] > parent[rootB]) { 
+//             parent[rootA] = rootB;
+//         } else if (parent[rootA] < parent[rootB]) {
+//             parent[rootB] = rootA;
+//         } else { 
+//             parent[rootA] += parent[rootB];
+//             parent[rootB] = rootA;
+//         }
+//     }
+// }
+
+// int main() {
+//     int t;
+//     scanf("%d", &t);
+
+//     while (t--) {
+//         int n, ops;
+//         scanf("%d %d", &n, &ops);
+//         parent = (int *)malloc(n * sizeof(int));
+
+//         for (int i = 0; i < n; i++) {
+//             parent[i] = -1;
+//         }
+
+//         for (int i = 0; i < ops; i++) {
+//             char command[10];
+//             int a, b;
+//             scanf("%s", command);
+            
+//             if (command[0] == 'u') { // union
+//                 scanf("%d %d", &a, &b);
+//                 heightUnion(a, b);
+//             } else if (command[0] == 'f') { // find
+//                 scanf("%d", &a);
+//                 printf("%d\n", find(a));
+//             } else if (command[0] == 's') { // same
+//                 scanf("%d %d", &a, &b);
+//                 if (find(a) == find(b)) printf("true\n");
+//                 else printf("false\n");
+//             }
+//         }
+
+//         free(parent);
+//     }
+
+//     return 0;
+// }
---
+++ Data_Structure/hw2/DS_hw3.c
@@ -0,0 +1,80 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#define MAX 360000
+int parent[MAX];
+
+typedef struct{
+    int source;
+    int dest;
+    int cost;
+} Edge;
+
+int find(int i){
+    for(; parent[i]>=0; i=parent[i])
+    ;
+    return i;
+}
+
+void heightUnion(int i, int j){
+    //i, j are roots with parent[i] = -height[i]
+    int root1 = find(i), root2 = find(j);
+    if(root1 > root2) // ex: -5 > -10
+        parent[root1] = root2;
+    else if (root1 == root2){
+        parent[root2] = root1;
+        parent[root1]--;
+    }
+    else
+        parent[root2] = root1;
+}
+
+int isSame(int e1, int e2){
+    if(find(e1)==find(e2))
+        return 1;
+    else 
+        return 0;
+}
+
+int compareEdges(const void* a, const void* b) {
+    return ((Edge*)a)->cost - ((Edge*)b)->cost;
+}
+
+void sortEdges(Edge *edges, long E){ // bubble sort
+    Edge temp;
+    for(int i=0; i<E; i++){
+        for(int j=0; j<E-i-1; j++){
+            if(edges[j].cost>edges[j+1].cost){
+                temp = edges[j];
+                edges[j]= edges[j+1];
+                edges[j+1] = temp;
+            }
+        }
+    }
+}
+
+int main(){
+    FILE * fp;
+    fp = fopen("data.txt", "r");
+    int V, E;
+    long long sum = 0;
+    scanf("%d %d",&V, &E);
+    // fscanf(fp, "%ld %ld",&V, &E);
+    for(int i=0; i<E; i++) // initialize
+        parent[i] = -1;
+    Edge edges[E];
+    for(int i=0; i<E; i++)
+        scanf("%d %d %d",&edges[i].source,&edges[i].dest,&edges[i].cost);
+        // fscanf(fp, "%d %d %d", &edges[i].source,&edges[i].dest,&edges[i].cost);
+    qsort(edges, E, sizeof(Edge), compareEdges);
+    for(int i=0; i<E; i++){
+        if(!(isSame(edges[i].source, edges[i].dest))){
+            sum += edges[i].cost;
+            heightUnion(edges[i].source, edges[i].dest);
+        }
+        else
+            continue;
+    }
+    printf("%lld", sum);
+    return 0;
+}
